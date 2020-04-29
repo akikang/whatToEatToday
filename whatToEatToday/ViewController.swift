c183f19486efbaf0ff8ff34953c1c84696effb11
cheng hang <akikang666@gmail.com>
2020-04-29 20:51:34 +0900

    加了个菜

--- whatToEatToday/ViewController.swift
+++ whatToEatToday/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     // menu
-    let menuList = ["烤鸡翅","油爆大虾","什锦炒蔬菜","香辣翅尖","青椒酿肉","汆烫","白菜肉丸","牛奶咖喱","铁板豆腐","椒盐小土豆","番茄牛腩","蒜蓉粉丝","韭菜馅饼"]
+    let menuList = ["烤鸡翅","油爆大虾","什锦炒蔬菜","香辣翅尖","青椒酿肉","汆烫","白菜肉丸","牛奶咖喱","铁板豆腐","椒盐小土豆","番茄牛腩","蒜蓉粉丝","泡菜五花肉"]
     // menu index
     var menuIndex = 0
     
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
     //***functions***
     //the func to change menu
     func changeMeanul(){
-        menuIndex = Int.random(in: 0...12)
+        menuIndex = Int.random(in: 0...13)
         mealLable.text = menuList[menuIndex]
     }
 
